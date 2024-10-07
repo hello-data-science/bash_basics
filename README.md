9004f9a0f0c0125e99bba06e2601986adbaedd04
rmgpanw <rmgpanw@ucl.ac.uk>
2024-10-07 22:59:40 +0100

    update assignment

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bash_basics
-Template repo for bash-related GitHub Classroom assignments
+Template repo for bash-related GitHub Classroom assignments.
+
+Task: Update `script.sh` to print the petal length of the 3rd versicolor observation in `iris.csv`
--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Path to the iris dataset
-iris_file="~/data/iris.csv"
-
 # Print the petal length of the 3rd versicolor observation 
 # Tip: use grep, cut, head and tail. 
 # Tip: first explore the header to ascertain which column records petal length
